13ea17052bbed7cfc1629b2a6e2581bc7fa6bcbc
j0nesma <matthewarjones@outlook.com>
2018-07-19 23:49:18 +0100

    init commit with api calls from a search

---
+++ src/pages/SearchPage.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+  <div id="searchBlock">
+    <b-card title="About Me">
+      <b-card-header>Search For Something</b-card-header>
+      <b-form @submit="onSubmit">
+        <b-form-input v-model="searchValue"
+                  type="text"
+                  placeholder="Enter your name"></b-form-input>
+        <b-button type="submit">search</b-button>
+      </b-form>
+      <p>{{searchValue}}</p>
+    </b-card>
+  </div>
+  <div v-if="result != []" v-for="res in result" :key="res.href">
+    {{res}}
+  </div>
+</div>
+</template>
+
+<script lang="js">
+import Vue from 'vue'
+import SearchService from '../utils/SearchService'
+
+export default Vue.extend({
+   name: 'searchPage',
+   
+   data () {
+    return {
+      searchValue: 'moon',
+      result:[]
+    }
+  },
+  methods:{ 
+    onSubmit (evt) {
+      let s = new SearchService();
+      evt.preventDefault();
+      s.getImage(this.searchValue, (res)=>{
+        this.result=res.collection.items
+      })
+    },
+   }
+    
+})
+</script>
+
+
+<style lang="scss">
+
+</style>
---
+++ src/utils/SearchService.js
@@ -0,0 +1,30 @@
+
+module.exports = function(searchValue){
+    const http = require('http');
+    const rootUrl = 'https://images-api.nasa.gov/';
+
+    let getImage = function(searchValue, callback){
+        console.log(searchValue);
+        let data = "";
+        req = http.get(rootUrl+"/search?q="+searchValue,(res)=>{
+            res.setEncoding('utf8');
+            res.on('data', function (d) {
+              data+=d;
+            });
+            res.on("end", function(){
+                console.log(JSON.parse(data))
+                callback(JSON.parse(data));
+            })
+          });
+          
+        req.on('error', function(e) {
+            console.log('problem with request: ' + e.message);
+        });
+    }
+
+    return {
+        'getImage':getImage
+    }
+}
+    
+

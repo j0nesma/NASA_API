5ca040a9ba2b08148cb172072deaae8c52173628
unknown <matthewarjones@outlook.com>
2018-07-22 22:47:30 +0100

    adding new components for use on the webpage and added place holder for Asset Page

---
+++ src/pages/AssestPage.vue
@@ -0,0 +1,24 @@
+<template>
+</template>
+
+<script lang="js">
+import Vue from 'vue'
+import SearchService from '../utils/SearchService'
+
+export default Vue.extend({
+   name: 'imageContainer',
+   
+   data () {
+    return {
+    }
+  },
+  methods:{ 
+   }
+    
+})
+</script>
+
+
+<style lang="scss">
+
+</style>
